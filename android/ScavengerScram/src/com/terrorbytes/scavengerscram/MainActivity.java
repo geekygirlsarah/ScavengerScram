f92e1ffcf70ffdf5465d70bd9362297be886b636
Sarah Peters <sarah.peters.developer@gmail.com>
2013-06-15 17:12:50 -0500

    Intent UserName

--- android/ScavengerScram/src/com/terrorbytes/scavengerscram/MainActivity.java
+++ android/ScavengerScram/src/com/terrorbytes/scavengerscram/MainActivity.java
@@ -1,13 +1,8 @@
 package com.terrorbytes.scavengerscram;
 
-import java.io.File;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -16,32 +11,30 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	TextView hiUsernameLabel;
-	public final static String USERNAME = "username";
+	//public final static String USERNAME = "username";
 	//public final static String GAME_CODE = "GAME_CODE";
 
 	String username;
 	String gameCode;
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	String playerName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 		hiUsernameLabel = (TextView) findViewById(R.id.hiUsernameLabel);
-
-		Intent loginIntent = new Intent(getApplicationContext(),
-				LoginActivity.class);
-
-		// loginIntent.putExtra(USERNAME, USERNAME);
-		startActivityForResult(loginIntent, 1);
+		
+		Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+		startActivity(loginIntent);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		if (data.getExtras().containsKey(USERNAME)) {
-			username = data.getExtras().getString(USERNAME);
+		if (data.getExtras().containsKey(IntentConstants.USERNAME)) {
+			username = data.getExtras().getString(IntentConstants.USERNAME);
 			hiUsernameLabel.setText("hi" + username);
 		}
 		else if(data.getExtras().containsKey(IntentConstants.GAME_CODE))
@@ -63,13 +56,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	// public void sendMessage(View view) {
-	//
-	// Intent intent = new Intent(this, LoginActivity.class);
-	// EditText editText = (EditText) findViewById(R.id.email);
-	// String message = editText.getText().toString();
-	// intent.putExtra(EXTRA_MESSAGE, message);
-	// }
+	
+	public void sendMessage(View view) {
+		
+		Intent intent = new Intent(this, LoginActivity.class);
+		EditText editText = (EditText) findViewById(R.id.email);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+	}
 
 }
