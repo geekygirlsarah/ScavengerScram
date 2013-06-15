5ecca0aeb008ee6296fafd148becec5ec4edf9eb
Anthony Schweigard <anthonyas007@gmail.com>
2013-06-15 18:53:58 -0500

    parse utils commit again

--- android/ScavengerScram/src/com/terrorbytes/scavengerscram/xml/ScavengerScramParseUtil.java
+++ android/ScavengerScram/src/com/terrorbytes/scavengerscram/xml/ScavengerScramParseUtil.java
@@ -1,17 +1,23 @@
 package com.terrorbytes.scavengerscram.xml;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringReader;
+
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import com.terrorbytes.scavengerscram.model.Game;
+import com.terrorbytes.scavengerscram.model.UserLogin;
 
 public class ScavengerScramParseUtil
-{
-	XPath xpath = XPathFactory.newInstance().newXPath();
-	
-	private static final String USERLOGIN_AUTHRESULT_EXPR = "//";
-	private static final String USERLOGIN_ID_EXPR = "";
-	private static final String USERLOGIN_NAME_EXPR = "";
+{	
+	private static final String BASE = "//ScavengerScram";
+	private static final String USERLOGIN_AUTHRESULT_EXPR = BASE + "/LoginAuth";
+	private static final String USERLOGIN_ID_EXPR = BASE + "/player_id";
+	private static final String USERLOGIN_NAME_EXPR = BASE + "/name";
 	
 	public static Game toGame(String xml)
 	{
@@ -19,10 +25,18 @@ public class ScavengerScramParseUtil
 		return null;	
 	}
 	
-	public static Game toUserLogin(String xml)
+	public static UserLogin toUserLogin(String xml) throws XPathExpressionException
 	{
 		XPath xpath = XPathFactory.newInstance().newXPath();
-		return null;	
+		String loginAuth = (String) xpath.evaluate(USERLOGIN_AUTHRESULT_EXPR, new StringReader(xml), XPathConstants.STRING);
+		String name = (String) xpath.evaluate(USERLOGIN_NAME_EXPR, new StringReader(xml), XPathConstants.STRING);
+		Integer playerId = (Integer) xpath.evaluate(USERLOGIN_ID_EXPR, new StringReader(xml), XPathConstants.NUMBER);
+		
+		UserLogin login = new UserLogin();
+		login.setAuthResult(loginAuth);
+		login.setName(name);
+		login.setId(playerId != null ? playerId.intValue() : -1);
+		return login;	
 	}
 
 }
