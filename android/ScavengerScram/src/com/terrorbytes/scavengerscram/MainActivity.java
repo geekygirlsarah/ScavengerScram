145692d6c3ee9eb02dcc41995c99c1dd5c1fcd33
Sarah Peters <sarah.peters.developer@gmail.com>
2013-06-15 11:53:37 -0500

    doing login

--- android/ScavengerScram/src/com/terrorbytes/scavengerscram/MainActivity.java
+++ android/ScavengerScram/src/com/terrorbytes/scavengerscram/MainActivity.java
@@ -1,11 +1,14 @@
 package com.terrorbytes.scavengerscram;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +21,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+//	public void sendMessage(View view) {
+//		
+//		Intent intent = new Intent(this, DisplayMessageActivity.class);
+//		EditText editText = (EditText) findViewById(R.id.usernameEditText);
+//		String message = editText.getText().toString();
+//		intent.putExtra(EXTRA_MESSAGE, message);
+//	}
 
 }
