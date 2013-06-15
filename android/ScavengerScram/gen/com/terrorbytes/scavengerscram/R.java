7ad94f546839cb0b68e910a7c117ac611b41f5f4
Sarah Peters <sarah.peters.developer@gmail.com>
2013-06-15 16:24:27 -0500

    strings
    strings

--- android/ScavengerScram/gen/com/terrorbytes/scavengerscram/R.java
+++ android/ScavengerScram/gen/com/terrorbytes/scavengerscram/R.java
@@ -24,26 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f080013;
-        public static final int action_settings=0x7f080012;
+        public static final int action_forgot_password=0x7f080012;
+        public static final int action_settings=0x7f080011;
         public static final int codeEditText=0x7f080001;
-        public static final int currentButton=0x7f08000e;
         public static final int email=0x7f080008;
         public static final int enterLabel=0x7f080004;
         public static final int hiUsernameLabel=0x7f08000c;
-        public static final int joinButton=0x7f08000f;
+        public static final int joinButton=0x7f08000e;
         public static final int linearLayout1=0x7f08000d;
         public static final int login=0x7f08000a;
         public static final int login_form=0x7f080007;
         public static final int login_status=0x7f080005;
         public static final int login_status_message=0x7f080006;
         public static final int password=0x7f080009;
-        public static final int pastButton=0x7f080010;
         public static final int playerNameEditText=0x7f080002;
         public static final int playerNameLabel=0x7f080000;
-        public static final int profileButton=0x7f080011;
+        public static final int profileButton=0x7f08000f;
         public static final int sign_in_button=0x7f08000b;
         public static final int submitButton=0x7f080003;
+        public static final int twoLineListItem1=0x7f080010;
     }
     public static final class layout {
         public static final int activity_join=0x7f030000;
