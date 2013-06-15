e7ee41fa25d5ca619516519d44fd5845f250db3e
Sarah Peters <sarah.peters.developer@gmail.com>
2013-06-15 15:59:57 -0500

    intents

---
+++ android/ScavengerScram/src/com/terrorbytes/scavengerscram/IntentConstants.java
@@ -0,0 +1,8 @@
+package com.terrorbytes.scavengerscram;
+
+public class IntentConstants {
+
+	public static final String GAME_CODE = "GAME_CODE";
+	public static final String USERNAME = "USERNAME";
+	public static final String PLAYER_NAME = "PLAYER_NAME";
+}
