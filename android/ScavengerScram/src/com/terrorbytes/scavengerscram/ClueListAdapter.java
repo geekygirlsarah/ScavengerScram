baa1a1c99f1fa7aa5a7290bb23b0c2ca2f4eb189
Sarah Peters <sarah.peters.developer@gmail.com>
2013-06-15 22:16:29 -0500

    clues!

---
+++ android/ScavengerScram/src/com/terrorbytes/scavengerscram/ClueListAdapter.java
@@ -0,0 +1,62 @@
+package com.terrorbytes.scavengerscram;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.terrorbytes.scavengerscram.model.Clue;
+
+public class ClueListAdapter extends BaseAdapter {
+	private ArrayList<Clue> clues;
+	private LayoutInflater mInflater;
+	
+	public ClueListAdapter(Context context, ArrayList<Clue> clues)
+	{
+		this.clues = clues;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	 public int getCount() {
+	  return clues.size();
+	 }
+
+	 public Object getItem(int position) {
+	  return clues.get(position);
+	 }
+
+	 public long getItemId(int position) {
+	  return position;
+	 }
+
+	 public View getView(int position, View convertView, ViewGroup parent) {
+	  ViewHolder holder;
+	  if (convertView == null) {
+	   convertView = mInflater.inflate(R.layout.clue_custom_row_view, null);
+	   holder = new ViewHolder();
+	   holder.ClueTitle = (TextView) convertView.findViewById(R.id.clueTitle);
+	   holder.ClueDescription = (TextView) convertView.findViewById(R.id.clueDescription);
+	  
+
+	   convertView.setTag(holder);
+	  } else {
+	   holder = (ViewHolder) convertView.getTag();
+	  }
+	  
+	  holder.ClueTitle.setText(clues.get(position).getTitle());
+	  holder.ClueDescription.setText(clues.get(position).getDescription());
+	
+
+	  return convertView;
+	 }
+
+	 static class ViewHolder {
+	  TextView ClueTitle;
+	  TextView ClueDescription;
+	  
+	 }
+	}
